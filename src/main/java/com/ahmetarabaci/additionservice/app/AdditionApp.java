cf1a819243889f86095101aea7853fe1dfa263d8
Ahmet Arabacı <ahmet.istbl.aa@gmail.com>
2025-02-23 17:23:50 +0300

    Eureka client has been added.

--- src/main/java/com/ahmetarabaci/additionservice/app/AdditionApp.java
+++ src/main/java/com/ahmetarabaci/additionservice/app/AdditionApp.java
@@ -2,10 +2,12 @@ package com.ahmetarabaci.additionservice.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ahmetarabaci.*"})
+@EnableDiscoveryClient
 public class AdditionApp {
 
 	public static void main(String[] args) {
